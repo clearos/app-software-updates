25c7e4b383c08e9361fd6259c9a43fe4dfd2dbee
clearostranslations <translations@clearfoundation.com>
2014-06-24 10:57:21 -0400

    Prepped for translation server

--- language/en_US/software_updates_lang.php
+++ language/en_US/software_updates_lang.php
@@ -1,38 +1,35 @@
 <?php
 
-$lang['software_updates_app_name'] = 'Software Updates';
 $lang['software_updates_app_description'] = 'The Software Updates app provides updates for the underlying operating system components.';
+$lang['software_updates_app_name'] = 'Software Updates';
 $lang['software_updates_automatic_updates'] = 'Automatic Updates';
-$lang['software_updates_upgrade'] = 'Upgrade';
-$lang['software_updates_package'] = 'Package';
-$lang['software_updates_repository'] = 'Repository';
 $lang['software_updates_available_updates'] = 'Available Updates';
-$lang['software_updates_type'] = 'Type';
-$lang['software_updates_install_progress'] = 'Install Progress';
-$lang['software_updates_progress_help'] = 'Your system is currently being updated.  This process can take some time, so please be patient!';
-$lang['software_updates_software_updates'] = 'Software Updates';
 $lang['software_updates_busy_message'] = 'Another user or service is currently using the software updates system.  When the process is complete, this app will become available again.';
-$lang['software_updates_software_updates_busy'] = 'Software Updates Busy';
-$lang['software_updates_loading_updates_message'] = 'Loading software updates list, please be patient.';
-
-$lang['software_updates_details'] = 'Details';
-$lang['software_updates_overall_progress'] = 'Overall Progress';
 $lang['software_updates_current_progress'] = 'Current Progress';
-$lang['software_updates_updates'] = 'Updates';
-$lang['software_updates_progress'] = 'Progress';
-$lang['software_updates_update_all'] = 'Update All';
+$lang['software_updates_details'] = 'Details';
+$lang['software_updates_install_progress'] = 'Install Progress';
+$lang['software_updates_loading_updates_message'] = 'Loading software updates list, please be patient.';
 $lang['software_updates_no_data_available'] = 'No data available.';
-$lang['software_updates_please_be_patient'] = 'Please be Patient!';
+$lang['software_updates_overall_progress'] = 'Overall Progress';
+$lang['software_updates_package'] = 'Package';
 $lang['software_updates_please_be_patient_detail'] = 'It can take a minute or two to update all the repository and update details.  This information is cached for future use.';
-$lang['software_updates_wizard_description'] = 'Now that your network is configured, critical updates to the core system are now installed.';
-$lang['software_updates_updates_complete'] = 'Software Updates Complete';
-$lang['software_updates_updates_complete_detail'] = 'The latest software updates have been installed.';
-$lang['software_updates_return_to_overview'] = 'Return to Overview';
-$lang['software_updates_system_is_up_to_date'] = 'System is up-to-date.';
-$lang['software_updates_software_update_in_progress'] = 'Software update is in progress.  Please come back later.';
-$lang['software_updates_software_up_to_date'] = 'Software Up-to-Date';
-$lang['software_updates_the_latest_software_updates_are_installed'] = 'The latest software updates are installed.';;
-
+$lang['software_updates_please_be_patient'] = 'Please be Patient!';
+$lang['software_updates_progress_help'] = 'Your system is currently being updated.  This process can take some time, so please be patient!';
+$lang['software_updates_progress'] = 'Progress';
 $lang['software_updates_recent_software_activity'] = 'Recent Software Activity';
+$lang['software_updates_repository'] = 'Repository';
+$lang['software_updates_return_to_overview'] = 'Return to Overview';
+$lang['software_updates_software_update_in_progress'] = 'Software update is in progress.  Please come back later.';
+$lang['software_updates_software_updates_busy'] = 'Software Updates Busy';
+$lang['software_updates_software_updates'] = 'Software Updates';
 $lang['software_updates_software_updates_system_busy'] = 'Software updates system is busy.';
-
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'System is up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'The latest software updates are installed.';;
+$lang['software_updates_type'] = 'Type';
+$lang['software_updates_update_all'] = 'Update All';
+$lang['software_updates_updates_complete_detail'] = 'The latest software updates have been installed.';
+$lang['software_updates_updates_complete'] = 'Software Updates Complete';
+$lang['software_updates_updates'] = 'Updates';
+$lang['software_updates_upgrade'] = 'Upgrade';
+$lang['software_updates_wizard_description'] = 'Now that your network is configured, critical updates to the core system are now installed.';
