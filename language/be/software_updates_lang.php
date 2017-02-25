0d6d8552159fad9dd562af5527cca5b1365a8346
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:37:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'дадатковыя абнаўлення';
+$lang['software_updates_app_description'] = 'Дадатак Абнаўленне праграмнага забеспячэння прадастаўляе абнаўлення для асноўных кампанентаў аперацыйнай сістэмы.';
+$lang['software_updates_app_name'] = 'Абнаўлення праграмнага забеспячэння';
+$lang['software_updates_automatic_updates'] = 'аўтаматычнае абнаўленне';
+$lang['software_updates_available_updates'] = 'даступныя абнаўлення';
+$lang['software_updates_busy_message'] = 'Іншы карыстальнік ці паслуга ў цяперашні час выкарыстоўвае сістэму абнаўленняў праграмнага забеспячэння. Калі працэс будзе завершаны, гэта дадатак стане даступным зноў.';
+$lang['software_updates_current_progress'] = 'бягучы прагрэс';
+$lang['software_updates_details'] = 'дэталі';
+$lang['software_updates_install_progress'] = 'ўсталяваць Прагрэс';
+$lang['software_updates_loading_updates_message'] = 'Загрузка спісу абнаўленняў праграмнага забеспячэння, калі ласка, будзьце цярплівыя.';
+$lang['software_updates_no_data_available'] = 'Дадзеных няма.';
+$lang['software_updates_overall_progress'] = 'агульны прагрэс';
+$lang['software_updates_package'] = 'пакет';
+$lang['software_updates_please_be_patient_detail'] = 'Гэта можа заняць хвіліну ці дзве, каб абнавіць усе сховішча і абнавіць дадзеныя. Гэтая інфармацыя кэшуецца для выкарыстання ў будучыні.';
+$lang['software_updates_please_be_patient'] = 'Калі ласка, будзьце цярплівыя!';
+$lang['software_updates_progress_help'] = 'Ваша сістэма ў цяперашні час абнаўляецца. Гэты працэс можа заняць некаторы час, таму, калі ласка, будзьце цярплівыя!';
+$lang['software_updates_progress'] = 'прагрэс';
+$lang['software_updates_recent_software_activity'] = 'Апошнія версіі праграмнага забеспячэння актыўнасць';
+$lang['software_updates_repository'] = 'ёмішча';
+$lang['software_updates_return_to_overview'] = 'Вярнуцца да аглядзе';
+$lang['software_updates_software_update_in_progress'] = 'Абнаўленне праграмнага забеспячэння ў працэсе. Калі ласка, зайдзіце пазней.';
+$lang['software_updates_software_updates_busy'] = 'Абнаўлення праграмнага забеспячэння занятасці';
+$lang['software_updates_software_updates'] = 'Абнаўлення праграмнага забеспячэння';
+$lang['software_updates_software_updates_system_busy'] = 'Сістэма абнаўленняў праграмнага забеспячэння занятая.';
+$lang['software_updates_software_up_to_date'] = 'Праграмнае забеспячэнне да сучасных';
+$lang['software_updates_system_is_up_to_date'] = 'Сістэма да сучасных.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Апошнія абнаўлення праграмнага забеспячэння ўстанаўліваюцца.';
+$lang['software_updates_type'] = 'тып';
+$lang['software_updates_update_all'] = 'абнавіць усе';
+$lang['software_updates_updates_complete'] = 'Абнаўлення праграмнага забеспячэння Поўная';
+$lang['software_updates_updates'] = 'абнаўлення';
+$lang['software_updates_upgrade'] = 'мадэрнізацыя';
+$lang['software_updates_wizard_description'] = 'Цяпер, калі сетка настроена, крытычныя абнаўлення ядра сістэмы цяпер ўстаноўлены.';
+$lang['software_updates_wizard_latest_necessary'] = 'Неабходна абнавіць сістэмнае праграмнае забеспячэнне, перш чым працягнуць.';
