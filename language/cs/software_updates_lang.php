fd7e211ffc6711ca79500bee35bfde74d6b31019
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:38:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'další aktualizace';
+$lang['software_updates_app_description'] = 'Aplikace Aktualizace softwaru poskytuje aktualizace pro podkladové součásti operačního systému.';
+$lang['software_updates_app_name'] = 'Aktualizace softwaru';
+$lang['software_updates_automatic_updates'] = 'Automatické aktualizace';
+$lang['software_updates_available_updates'] = 'k dispozici aktualizace';
+$lang['software_updates_busy_message'] = 'Jiný uživatel nebo služba je v současné době používá systém aktualizací softwaru. Když je proces dokončen, bude tato aplikace opět k dispozici.';
+$lang['software_updates_current_progress'] = 'Aktuální Progress';
+$lang['software_updates_details'] = 'podrobnosti';
+$lang['software_updates_install_progress'] = 'nainstalovat Progress';
+$lang['software_updates_loading_updates_message'] = 'Vkládání seznam aktualizací softwaru, prosím, buďte trpěliví.';
+$lang['software_updates_no_data_available'] = 'Žádné údaje nejsou k dispozici.';
+$lang['software_updates_overall_progress'] = 'Celkový postup';
+$lang['software_updates_package'] = 'Balík';
+$lang['software_updates_please_be_patient_detail'] = 'To může trvat minutu nebo dvě aktualizovat všechny úložiště a aktualizovat údaje. Tyto informace jsou ukládány do mezipaměti pro budoucí použití.';
+$lang['software_updates_please_be_patient'] = 'Prosím, buďte trpěliví!';
+$lang['software_updates_progress_help'] = 'Váš systém je v současné době aktualizován. Tento proces může trvat nějakou dobu, takže buďte trpěliví!';
+$lang['software_updates_progress'] = 'Pokrok';
+$lang['software_updates_recent_software_activity'] = 'Nedávná aktivita Software';
+$lang['software_updates_repository'] = 'sklad';
+$lang['software_updates_return_to_overview'] = 'Zpět na přehled';
+$lang['software_updates_software_update_in_progress'] = 'Aktualizace softwaru probíhá. Prosím, vraťte se později.';
+$lang['software_updates_software_updates_busy'] = 'Aktualizace softwaru Busy';
+$lang['software_updates_software_updates'] = 'Aktualizace softwaru';
+$lang['software_updates_software_updates_system_busy'] = 'Aktualizace softwaru Systém je zaneprázdněn.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'Systém je up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Nejnovější aktualizace softwaru jsou instalovány.';
+$lang['software_updates_type'] = 'Typ';
+$lang['software_updates_update_all'] = 'Aktualizovat vše';
+$lang['software_updates_updates_complete'] = 'Aktualizace softwaru Complete';
+$lang['software_updates_updates'] = 'aktualizace';
+$lang['software_updates_upgrade'] = 'Vylepšit';
+$lang['software_updates_wizard_description'] = 'Nyní, když je vaše síť konfigurována, kritické aktualizace jádra systému jsou nyní nainstalovány.';
+$lang['software_updates_wizard_latest_necessary'] = 'Před pokračováním Prosím aktualizujte systémový software.';
