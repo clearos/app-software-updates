b16134047733476ffd033f11d37ae6d1adcc6ae8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:42:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'papildu atjauninājumi';
+$lang['software_updates_app_description'] = 'Programmatūra Updates app sniedz atjauninājumus pamata operētājsistēmu komponentiem.';
+$lang['software_updates_app_name'] = 'programmatūras atjauninājumi';
+$lang['software_updates_automatic_updates'] = 'Automatic Updates';
+$lang['software_updates_available_updates'] = 'pieejami atjauninājumi';
+$lang['software_updates_busy_message'] = 'Cits lietotājs vai pakalpojums pašlaik izmanto programmatūras atjauninājumus sistēmu. Kad process ir pabeigts, šī app būs pieejami atkal.';
+$lang['software_updates_current_progress'] = 'pašreizējais Progress';
+$lang['software_updates_details'] = 'Sīkāka informācija';
+$lang['software_updates_install_progress'] = 'instalējiet Progress';
+$lang['software_updates_loading_updates_message'] = 'Iekraušanas programmatūras atjauninājumus sarakstu, lūdzu, esiet pacietīgi.';
+$lang['software_updates_no_data_available'] = 'Nav pieejami dati.';
+$lang['software_updates_overall_progress'] = 'Kopumā Progress';
+$lang['software_updates_package'] = 'paka';
+$lang['software_updates_please_be_patient_detail'] = 'Tas var aizņemt minūti vai divas, lai atjauninātu visu glabātavu un atjaunināt informāciju. Šī informācija ir kešatmiņā turpmākai izmantošanai.';
+$lang['software_updates_please_be_patient'] = 'Lūdzu, esiet pacietīgi!';
+$lang['software_updates_progress_help'] = 'Jūsu sistēma pašlaik tiek atjaunināts. Šis process var aizņemt kādu laiku, tāpēc, lūdzu, esiet pacietīgi!';
+$lang['software_updates_progress'] = 'progress';
+$lang['software_updates_recent_software_activity'] = 'Nesenā Software aktivitāte';
+$lang['software_updates_repository'] = 'krātuve';
+$lang['software_updates_return_to_overview'] = 'Atgriezties uz pārskatu';
+$lang['software_updates_software_update_in_progress'] = 'Programmatūras atjaunināšana ir progress. Lūdzu atgriezties vēlāk.';
+$lang['software_updates_software_updates_busy'] = 'Software Updates Aizņemts';
+$lang['software_updates_software_updates'] = 'programmatūras atjauninājumi';
+$lang['software_updates_software_updates_system_busy'] = 'Programmatūras atjauninājumi sistēma ir aizņemta.';
+$lang['software_updates_software_up_to_date'] = 'Programmatūras up-to-dienas';
+$lang['software_updates_system_is_up_to_date'] = 'Sistēma ir up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Jaunāko programmatūras atjauninājumi tiek instalēti.';
+$lang['software_updates_type'] = 'tips';
+$lang['software_updates_update_all'] = 'Update All';
+$lang['software_updates_updates_complete'] = 'Software Updates Complete';
+$lang['software_updates_updates'] = 'Updates';
+$lang['software_updates_upgrade'] = 'Upgrade';
+$lang['software_updates_wizard_description'] = 'Tagad, kad jūsu tīkls ir konfigurēts, kritiskos atjauninājumus galveno sistēmas uzinstalēti.';
+$lang['software_updates_wizard_latest_necessary'] = 'Pirms turpināt, lūdzu, atjauniniet savu sistēmas programmatūru.';
