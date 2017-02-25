fc11f3b67d7486f3f849d598a2227a078f064181
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:40:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'अतिरिक्त अद्यतन';
+$lang['software_updates_app_description'] = 'सॉफ्टवेयर अपडेट एप्लिकेशन अंतर्निहित ऑपरेटिंग सिस्टम घटकों के लिए अद्यतन उपलब्ध कराता है।';
+$lang['software_updates_app_name'] = 'सॉफ्टवेयर अपडेट';
+$lang['software_updates_automatic_updates'] = 'स्वचालित अद्यतन';
+$lang['software_updates_available_updates'] = 'उपलब्ध अपडेट';
+$lang['software_updates_busy_message'] = 'किसी अन्य उपयोगकर्ता या सेवा वर्तमान में सॉफ्टवेयर अद्यतन प्रणाली का उपयोग किया जाता है। प्रक्रिया पूर्ण होने पर, इस एप्लिकेशन को फिर से उपलब्ध हो जाएगा।';
+$lang['software_updates_current_progress'] = 'वर्तमान प्रगति';
+$lang['software_updates_details'] = 'विवरण';
+$lang['software_updates_install_progress'] = 'अधिष्ठापन प्रगति';
+$lang['software_updates_loading_updates_message'] = 'लोड हो रहा है सॉफ्टवेयर अद्यतन सूची के लिए, कृपया धैर्य रखें।';
+$lang['software_updates_no_data_available'] = 'कोई डेटा उपलब्ध नहीं है।';
+$lang['software_updates_overall_progress'] = 'समग्र प्रगति';
+$lang['software_updates_package'] = 'पैकेज';
+$lang['software_updates_please_be_patient_detail'] = 'यह सब रिपोजिटरी और अद्यतन विवरण को अद्यतन करने के लिए एक या दो मिनट ले जा सकते हैं। यह जानकारी भविष्य में उपयोग के लिए कैश है।';
+$lang['software_updates_please_be_patient'] = 'कृपया धैर्य रखें!';
+$lang['software_updates_progress_help'] = 'आपका सिस्टम वर्तमान में अद्यतन किया जा रहा है। इस प्रक्रिया में कुछ समय लग सकता है, तो कृपया धैर्य रखें!';
+$lang['software_updates_progress'] = 'प्रगति';
+$lang['software_updates_recent_software_activity'] = 'हाल के सॉफ्टवेयर गतिविधि';
+$lang['software_updates_repository'] = 'कोष';
+$lang['software_updates_return_to_overview'] = 'अवलोकन करने के लिए लौटें';
+$lang['software_updates_software_update_in_progress'] = 'सॉफ्टवेयर अद्यतन प्रगति पर है। कृपया बाद में आइये।';
+$lang['software_updates_software_updates_busy'] = 'सॉफ्टवेयर अपडेट व्यस्त';
+$lang['software_updates_software_updates'] = 'सॉफ्टवेयर अपडेट';
+$lang['software_updates_software_updates_system_busy'] = 'सॉफ्टवेयर अद्यतन प्रणाली व्यस्त है।';
+$lang['software_updates_software_up_to_date'] = 'अप-टू-डेट सॉफ्टवेयर';
+$lang['software_updates_system_is_up_to_date'] = 'प्रणाली अप-टू-डेट है।';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'नवीनतम सॉफ्टवेयर अद्यतन स्थापित हैं।';
+$lang['software_updates_type'] = 'प्रकार';
+$lang['software_updates_update_all'] = 'सब अद्यतित';
+$lang['software_updates_updates_complete'] = 'सॉफ्टवेयर अपडेट पूरा';
+$lang['software_updates_updates'] = 'अपडेट';
+$lang['software_updates_upgrade'] = 'नवीनीकरण';
+$lang['software_updates_wizard_description'] = 'अब जब कि अपने नेटवर्क विन्यस्त है, कोर प्रणाली के लिए महत्वपूर्ण अद्यतन अब स्थापित कर रहे हैं।';
+$lang['software_updates_wizard_latest_necessary'] = 'जारी रखने से पहले अपने सिस्टम सॉफ्टवेयर अपडेट करें।';
