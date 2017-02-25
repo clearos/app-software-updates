95bfcb02b4fc84795b479901057ddcaed97fa155
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:43:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'kemas kini tambahan';
+$lang['software_updates_app_description'] = 'Updates Software app menyediakan maklumat terkini bagi komponen sistem operasi asas.';
+$lang['software_updates_app_name'] = 'Update perisian';
+$lang['software_updates_automatic_updates'] = 'Kemas Kini automatik';
+$lang['software_updates_available_updates'] = 'Terkini di';
+$lang['software_updates_busy_message'] = 'Seorang lagi pengguna atau perkhidmatan menggunakan sistem kemas kini perisian. Apabila proses ini selesai, aplikasi ini akan menjadi didapati lagi.';
+$lang['software_updates_current_progress'] = 'Kemajuan Semasa';
+$lang['software_updates_details'] = 'butiran';
+$lang['software_updates_install_progress'] = 'Pasang Progress';
+$lang['software_updates_loading_updates_message'] = 'Memuatkan senarai kemas kini perisian, sila bersabar.';
+$lang['software_updates_no_data_available'] = 'Tiada data.';
+$lang['software_updates_overall_progress'] = 'Kemajuan keseluruhan';
+$lang['software_updates_package'] = 'pakej';
+$lang['software_updates_please_be_patient_detail'] = 'Ia boleh mengambil masa satu atau dua minit untuk mengemas kini semua repositori dan maklumat lanjut. Maklumat ini cache untuk kegunaan masa depan.';
+$lang['software_updates_please_be_patient'] = 'Sila bersabar!';
+$lang['software_updates_progress_help'] = 'Sistem anda sedang dikemaskini. Proses ini boleh mengambil sedikit masa, jadi sila bersabar!';
+$lang['software_updates_progress'] = 'kemajuan';
+$lang['software_updates_recent_software_activity'] = 'Software terkini Aktiviti';
+$lang['software_updates_repository'] = 'Repository';
+$lang['software_updates_return_to_overview'] = 'Kembali ke Gambaran Keseluruhan';
+$lang['software_updates_software_update_in_progress'] = 'kemas kini perisian sedang dijalankan. Sila datang lagi kemudian.';
+$lang['software_updates_software_updates_busy'] = 'Perisian Terbaru Busy';
+$lang['software_updates_software_updates'] = 'Update perisian';
+$lang['software_updates_software_updates_system_busy'] = 'sistem kemas kini perisian sedang sibuk.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'Sistem adalah up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Kemas kini perisian terkini dipasang.';
+$lang['software_updates_type'] = 'Jenis';
+$lang['software_updates_update_all'] = 'Kemaskini semua';
+$lang['software_updates_updates_complete'] = 'Perisian Terbaru Lengkap';
+$lang['software_updates_updates'] = 'kemas kini';
+$lang['software_updates_upgrade'] = 'Naik taraf';
+$lang['software_updates_wizard_description'] = 'Sekarang rangkaian anda dikonfigurasikan, kemas kini yang penting untuk sistem teras kini dipasang.';
+$lang['software_updates_wizard_latest_necessary'] = 'Sila kemas kini perisian sistem anda sebelum meneruskan.';
