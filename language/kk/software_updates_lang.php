5ac6773cf63a5f5cc811566953629b9204127628
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:41:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'қосымша жаңартулар';
+$lang['software_updates_app_description'] = 'Бағдарламалық қамтамасыз ету Жаңартулар Қолданба базалық операциялық жүйе компоненттері үшін жаңартуларды қамтамасыз етеді.';
+$lang['software_updates_app_name'] = 'Бағдарламалық қамтамасыз ету жаңартулар';
+$lang['software_updates_automatic_updates'] = 'Автоматты жаңартулар';
+$lang['software_updates_available_updates'] = 'қол жетімді жаңартуларды';
+$lang['software_updates_busy_message'] = 'Басқа пайдаланушы немесе қызмет қазіргі уақытта бағдарламалық жасақтама жаңартулары жүйесін қолдануда. процесі аяқталған соң, осы бағдарлама қайтадан қолжетімді болады.';
+$lang['software_updates_current_progress'] = 'Ағымдағы Прогресс';
+$lang['software_updates_details'] = 'Егжей';
+$lang['software_updates_install_progress'] = 'Прогресс орнату';
+$lang['software_updates_loading_updates_message'] = 'бағдарламалық құрал жаңартулары тізімін жүктеу, өтінеміз шыдамды.';
+$lang['software_updates_no_data_available'] = 'қолда бар деректер жоқ.';
+$lang['software_updates_overall_progress'] = 'Жалпы Прогресс';
+$lang['software_updates_package'] = 'пакет';
+$lang['software_updates_please_be_patient_detail'] = 'Ол барлық репозитарийді және жаңарту мәліметтерін жаңарту үшін бір немесе екі минут кетуі мүмкін. Бұл ақпарат болашақта пайдалану үшін бүркемеленген.';
+$lang['software_updates_please_be_patient'] = 'Өтінеміз, шыдамды болыңыз!';
+$lang['software_updates_progress_help'] = 'Сіздің жүйесі қазіргі кезде жаңартылуда. Бұл процесс біраз уақыт алуы мүмкін, сондықтан шыдамды болыңыз өтінеміз!';
+$lang['software_updates_progress'] = 'прогресс';
+$lang['software_updates_recent_software_activity'] = 'Соңғы бағдарламалық құрал Қызметі';
+$lang['software_updates_repository'] = 'Репозиторий';
+$lang['software_updates_return_to_overview'] = 'Шолу оралу';
+$lang['software_updates_software_update_in_progress'] = 'Бағдарламалық қамтамасыз етуді жаңарту жүргізілуде. Кейінірек қайта кіріңіз.';
+$lang['software_updates_software_updates_busy'] = 'Бағдарламалық қамтамасыз ету Жаңартулар Busy';
+$lang['software_updates_software_updates'] = 'Бағдарламалық қамтамасыз ету жаңартулар';
+$lang['software_updates_software_updates_system_busy'] = 'Бағдарламалық құрал жаңартулары жүйесі бос емес.';
+$lang['software_updates_software_up_to_date'] = 'Бағдарламалық қамтамасыз ету Up заманауи';
+$lang['software_updates_system_is_up_to_date'] = 'Жүйе-ақ күні болып табылады.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Соңғы жаңарту орнатылған.';
+$lang['software_updates_type'] = 'түрі';
+$lang['software_updates_update_all'] = 'Барлық жаңарту';
+$lang['software_updates_updates_complete'] = 'Толық бағдарламалық құралдарды жаңарту';
+$lang['software_updates_updates'] = 'жаңартулар';
+$lang['software_updates_upgrade'] = 'Жаңалау';
+$lang['software_updates_wizard_description'] = 'Желі конфигурациясының деп Енді негізгі жүйесіне сыни жаңартулар қазір орнатылған.';
+$lang['software_updates_wizard_latest_necessary'] = 'жалғастырмас бұрын сіздің жүйе бағдарламалық құралын жаңартыңыз.';
