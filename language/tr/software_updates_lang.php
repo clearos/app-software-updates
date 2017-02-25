1a5e20febcc4d509e91899e2f3d464d4d8ec1e74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:46:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'ek güncellemeler';
+$lang['software_updates_app_description'] = 'Yazılım Güncellemeleri uygulaması temel işletim sistemi bileşenleri için güncelleştirmeler sağlar.';
+$lang['software_updates_app_name'] = 'Yazılım güncellemeleri';
+$lang['software_updates_automatic_updates'] = 'Otomatik güncellemeler';
+$lang['software_updates_available_updates'] = 'mevcut Güncellemeler';
+$lang['software_updates_busy_message'] = 'Başka bir kullanıcı veya hizmet şu anda yazılım güncellemeleri sistemini kullanıyor. İşlem tamamlandığında, bu uygulama yeniden kullanılabilir hale gelecektir.';
+$lang['software_updates_current_progress'] = 'Güncel İlerleme';
+$lang['software_updates_details'] = 'ayrıntılar';
+$lang['software_updates_install_progress'] = 'İlerleme yükleyin';
+$lang['software_updates_loading_updates_message'] = 'yazılım güncellemeleri listesi yükleniyor, lütfen sabırlı olun.';
+$lang['software_updates_no_data_available'] = 'Veri yok.';
+$lang['software_updates_overall_progress'] = 'Genel ilerleme';
+$lang['software_updates_package'] = 'paket';
+$lang['software_updates_please_be_patient_detail'] = 'Tüm depo ve güncelleme ayrıntılarını güncellemek için bir ya da iki dakika sürebilir. Bu bilgiler gelecekte kullanılmak üzere önbelleğe alınır.';
+$lang['software_updates_please_be_patient'] = 'Lütfen sabırlı olun!';
+$lang['software_updates_progress_help'] = 'Sisteminiz şu anda güncellenmektedir. Bu işlem biraz zaman alabilir, bu yüzden lütfen sabırlı olun olabilir!';
+$lang['software_updates_progress'] = 'İlerleme';
+$lang['software_updates_recent_software_activity'] = 'Son Yazılım Etkinlik';
+$lang['software_updates_repository'] = 'depo';
+$lang['software_updates_return_to_overview'] = 'Özete geri dön';
+$lang['software_updates_software_update_in_progress'] = 'Yazılım güncellemesi devam ediyor. Lütfen daha sonra tekrar.';
+$lang['software_updates_software_updates_busy'] = 'Yazılım Güncellemeleri Meşgul';
+$lang['software_updates_software_updates'] = 'Yazılım güncellemeleri';
+$lang['software_updates_software_updates_system_busy'] = 'Yazılım güncellemeleri sistem meşgul.';
+$lang['software_updates_software_up_to_date'] = 'Yazılım Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'Sistem up-to-date olduğunu.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'En son yazılım güncellemeleri yüklenir.';
+$lang['software_updates_type'] = 'tip';
+$lang['software_updates_update_all'] = 'güncelleme Hepsi';
+$lang['software_updates_updates_complete'] = 'Yazılım Güncellemeleri Tamamlandı';
+$lang['software_updates_updates'] = 'Güncellemeler';
+$lang['software_updates_upgrade'] = 'yükseltmek';
+$lang['software_updates_wizard_description'] = 'Şimdi ağ yapılandırıldığını, çekirdek sistem kritik güncelleştirmeleri şimdi yüklenir.';
+$lang['software_updates_wizard_latest_necessary'] = 'Devam etmeden önce sistem yazılımını güncelleyin.';
