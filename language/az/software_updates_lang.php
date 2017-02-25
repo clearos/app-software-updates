e6a3950a9553adc2211a13a831bbfad90fe63b94
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:36:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'əlavə yenilikləri';
+$lang['software_updates_app_description'] = 'Software Updates app əsas əməliyyat sistemi komponentləri üçün yenilikləri təmin edir.';
+$lang['software_updates_app_name'] = 'Software Updates';
+$lang['software_updates_automatic_updates'] = 'Avtomatik Updates';
+$lang['software_updates_available_updates'] = 'Mövcud Updates';
+$lang['software_updates_busy_message'] = 'Başqa bir istifadəçi və ya xidmət hazırda proqram yenilikləri sistemi istifadə edir. prosesi başa zaman, bu app yenidən mövcud olacaq.';
+$lang['software_updates_current_progress'] = 'Cari Progress';
+$lang['software_updates_details'] = 'Ətraflı';
+$lang['software_updates_install_progress'] = 'Tərəqqi Yükle';
+$lang['software_updates_loading_updates_message'] = 'proqram yenilikləri siyahısı Loading, səbr edin.';
+$lang['software_updates_no_data_available'] = 'No data mövcuddur.';
+$lang['software_updates_overall_progress'] = 'Ümumi Progress';
+$lang['software_updates_package'] = 'Paket';
+$lang['software_updates_please_be_patient_detail'] = 'Bu, bütün depo və yeniləmə detalları güncellemek üçün bir dəqiqə və ya iki bilər. Bu məlumat gələcək istifadə üçün cached edir.';
+$lang['software_updates_please_be_patient'] = 'Səbr edin!';
+$lang['software_updates_progress_help'] = 'Sistem hazırda yenilənir olunur. Bu proses müəyyən vaxt tələb, belə ki, səbr edin bilər!';
+$lang['software_updates_progress'] = 'tərəqqi';
+$lang['software_updates_recent_software_activity'] = 'Son Software Fəaliyyət';
+$lang['software_updates_repository'] = 'Fondu';
+$lang['software_updates_return_to_overview'] = 'Baxış qayıt';
+$lang['software_updates_software_update_in_progress'] = 'Software yeniləmə davam edir. sonra geri gəlmək edin.';
+$lang['software_updates_software_updates_busy'] = 'Software Updates Busy';
+$lang['software_updates_software_updates'] = 'Software Updates';
+$lang['software_updates_software_updates_system_busy'] = 'Software yenilikləri sistem məşğul olur.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-tarix';
+$lang['software_updates_system_is_up_to_date'] = 'System up-to-tarix deyil.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'son proqram yenilikləri quraşdırılıb.';
+$lang['software_updates_type'] = 'Type';
+$lang['software_updates_update_all'] = 'Update All';
+$lang['software_updates_updates_complete'] = 'Software Updates Complete';
+$lang['software_updates_updates'] = 'Updates';
+$lang['software_updates_upgrade'] = 'təkmilləşdirmək';
+$lang['software_updates_wizard_description'] = 'İndi şəbəkə konfiqurasiya ki, əsas sistemi üçün kritik yenilikləri indi quraşdırılıb.';
+$lang['software_updates_wizard_latest_necessary'] = 'davam əvvəl sistem proqram güncelleyin.';
