2d0449b02fb12cbafc8568f88463832607b86a2e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:40:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'nuashonruithe breise';
+$lang['software_updates_app_description'] = 'Soláthraíonn an app Bogearraí Nuashonruithe nuashonruithe do na comhpháirteanna córais oibriúcháin is bun.';
+$lang['software_updates_app_name'] = 'Bogearraí Nuashonruithe';
+$lang['software_updates_automatic_updates'] = 'Uathoibríoch Nuashonruithe';
+$lang['software_updates_available_updates'] = 'Ar fáil Nuashonruithe';
+$lang['software_updates_busy_message'] = 'Tá úsáideoir nó seirbhís eile a mbeidh an gcóras nuashonruithe bogearraí láthair. Nuair a bhíonn an próiseas críochnaithe, beidh an app ar fáil arís.';
+$lang['software_updates_current_progress'] = 'Dul Chun Cinn atá ann faoi láthair';
+$lang['software_updates_details'] = 'Mionsonraí';
+$lang['software_updates_install_progress'] = 'Suiteáil Dul Chun Cinn';
+$lang['software_updates_loading_updates_message'] = 'Loading liosta nuashonruithe bogearraí, le do thoil a n-othar.';
+$lang['software_updates_no_data_available'] = 'Níl sonraí ar fáil.';
+$lang['software_updates_overall_progress'] = 'Dul Chun Cinn foriomlán';
+$lang['software_updates_package'] = 'pacáiste';
+$lang['software_updates_please_be_patient_detail'] = 'Is féidir é a chur le nóiméad nó dhó chun na sonraí stór agus a nuashonrú a nuashonrú. Tá an fhaisnéis seo i dtaisce le haghaidh úsáid sa todhchaí.';
+$lang['software_updates_please_be_patient'] = 'Tabhair faoi a bheith Othar!';
+$lang['software_updates_progress_help'] = 'Tá do chóras á nuashonrú faoi láthair. Is féidir an próiseas a ghlacadh roinnt ama, le do thoil a n-othar!';
+$lang['software_updates_progress'] = 'Dul chun cinn';
+$lang['software_updates_recent_software_activity'] = 'Gníomhaíocht Bogearraí Déanaí';
+$lang['software_updates_repository'] = 'stór';
+$lang['software_updates_return_to_overview'] = 'Fill ar Forbhreathnú';
+$lang['software_updates_software_update_in_progress'] = 'Tá Bogearraí cothrom le dáta ar siúl. Thoil teacht ar ais ina dhiaidh sin.';
+$lang['software_updates_software_updates_busy'] = 'Bogearraí Nuashonruithe Gnóthach';
+$lang['software_updates_software_updates'] = 'Bogearraí Nuashonruithe';
+$lang['software_updates_software_updates_system_busy'] = 'Tá córas nuashonruithe bogearraí gnóthach.';
+$lang['software_updates_software_up_to_date'] = 'Bogearraí cothrom le dáta';
+$lang['software_updates_system_is_up_to_date'] = 'Is córas cothrom le dáta.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Na nuashonruithe bogearraí is déanaí a shuiteáil.';
+$lang['software_updates_type'] = 'cineál';
+$lang['software_updates_update_all'] = 'Nuashonraigh Gach';
+$lang['software_updates_updates_complete'] = 'Bogearraí Nuashonruithe Comhlánaigh';
+$lang['software_updates_updates'] = 'Nuashonruithe';
+$lang['software_updates_upgrade'] = 'uasghrádú';
+$lang['software_updates_wizard_description'] = 'Anois go bhfuil do líonra cumraithe, nuashonruithe criticiúil le croíchóras atá suiteáilte anois.';
+$lang['software_updates_wizard_latest_necessary'] = 'Tabhair suas chun dáta do bhogearraí córais roimh leanúint.';
