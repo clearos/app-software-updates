b1220725afe5ae54842efa7d24f695e47bf12855
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:46:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'கூடுதல் மேம்படுத்தல்கள்';
+$lang['software_updates_app_description'] = 'மென்பொருள் மேம்படுத்தல்கள் பயன்பாட்டை அடிப்படை இயக்க முறைமை கூறுகள் மேம்படுத்தல்கள் வழங்குகிறது.';
+$lang['software_updates_app_name'] = 'மென்பொருள் மேம்படுத்தல்கள்';
+$lang['software_updates_automatic_updates'] = 'தானாக புதுப்பித்தலை';
+$lang['software_updates_available_updates'] = 'கிடைக்கும் மேம்படுத்தல்கள்';
+$lang['software_updates_busy_message'] = 'மற்றொரு பயனர் அல்லது சேவையை தற்போது மென்பொருள் மேம்படுத்தல்கள் அமைப்பு பயன்படுத்தி வருகிறது. செயல்முறை நிறைவடையும் போது, இந்த பயன்பாட்டை மீண்டும் கிடைக்கும்.';
+$lang['software_updates_current_progress'] = 'தற்போதைய முன்னேற்றம்';
+$lang['software_updates_details'] = 'விவரங்கள்';
+$lang['software_updates_install_progress'] = 'நிறுவல் வளர்ச்சி';
+$lang['software_updates_loading_updates_message'] = 'ஏற்றுகிறது மென்பொருள் மேம்படுத்தல்கள் பட்டியல், பொறுமை, தயவு செய்து.';
+$lang['software_updates_no_data_available'] = 'தரவு இல்லை.';
+$lang['software_updates_overall_progress'] = 'ஒட்டுமொத்த முன்னேற்றம்';
+$lang['software_updates_package'] = 'தொகுப்பு';
+$lang['software_updates_please_be_patient_detail'] = 'இது அனைத்து களஞ்சியம் மற்றும் மேம்படுத்தல் விவரங்கள் மேம்படுத்த ஒரு நிமிடம் அல்லது இரண்டு எடுக்க முடியும். இந்த தகவல் எதிர்கால பயன்பாட்டிற்காக இடைமாற்றை.';
+$lang['software_updates_please_be_patient'] = 'தயவுசெய்து பொருமைையாயிறு!';
+$lang['software_updates_progress_help'] = 'உங்கள் கணினி தற்போது புதுப்பிக்கப்படுகிறது. இந்த செயல்முறை சில நேரம் ஆகலாம், மிகவும் பொறுமையாக இருக்க, தயவு செய்து!';
+$lang['software_updates_progress'] = 'முன்னேற்றம்';
+$lang['software_updates_recent_software_activity'] = 'சமீபத்திய மென்பொருள் நடவடிக்கை';
+$lang['software_updates_repository'] = 'களஞ்சியம்';
+$lang['software_updates_return_to_overview'] = 'மேலோட்டத்திற்கு திரும்பு';
+$lang['software_updates_software_update_in_progress'] = 'மென்பொருள் மேம்படுத்தல் முன்னேற்றம் உள்ளது. பின்னர் மீண்டும் வந்து.';
+$lang['software_updates_software_updates_busy'] = 'மென்பொருள் மேம்படுத்தல்கள் பிஸி';
+$lang['software_updates_software_updates'] = 'மென்பொருள் மேம்படுத்தல்கள்';
+$lang['software_updates_software_updates_system_busy'] = 'மென்பொருள் மேம்படுத்தல்கள் அமைப்பு பிஸியாக இருக்கிறார்.';
+$lang['software_updates_software_up_to_date'] = 'மென்பொருள் அப் தேதி';
+$lang['software_updates_system_is_up_to_date'] = 'கணினி வரை தேதி ஆகும்.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'சமீபத்திய மென்பொருள் மேம்படுத்தல்கள் நிறுவப்படும்.';
+$lang['software_updates_type'] = 'வகை';
+$lang['software_updates_update_all'] = 'புதுப்பிக்கப்பட்டது அனைத்து';
+$lang['software_updates_updates_complete'] = 'மென்பொருள் மேம்படுத்தல்கள் முழுமையான';
+$lang['software_updates_updates'] = 'மேம்படுத்தல்கள்';
+$lang['software_updates_upgrade'] = 'மேம்படுத்தல்';
+$lang['software_updates_wizard_description'] = 'இப்போது உங்கள் பிணைய கட்டமைக்கப்பட்ட என்று, மைய அமைப்பு முக்கியமான புதுப்பிப்புகளைச் தற்போது நிறுவப்பட்டுள்ளன.';
+$lang['software_updates_wizard_latest_necessary'] = 'தொடர்வதற்கு முன் உங்கள் மென்பொருள் புதுப்பிக்கவும்.';
