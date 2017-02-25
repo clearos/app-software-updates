f1f8b9176209811211071201aeb8b3f2c01c03dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:38:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'yderligere opdateringer';
+$lang['software_updates_app_description'] = 'Den Softwareopdateringer app giver opdateringer til det underliggende operativsystem komponenter.';
+$lang['software_updates_app_name'] = 'Software opdateringer';
+$lang['software_updates_automatic_updates'] = 'automatiske opdateringer';
+$lang['software_updates_available_updates'] = 'tilgængelige opdateringer';
+$lang['software_updates_busy_message'] = 'En anden bruger eller tjeneste er i øjeblikket ved hjælp af softwareopdateringer systemet. Når processen er færdig, vil denne app bliver tilgængelig igen.';
+$lang['software_updates_current_progress'] = 'Aktuel Progress';
+$lang['software_updates_details'] = 'detaljer';
+$lang['software_updates_install_progress'] = 'Installer Progress';
+$lang['software_updates_loading_updates_message'] = 'Loading softwareopdateringer liste, vær tålmodig.';
+$lang['software_updates_no_data_available'] = 'Ingen data til rådighed.';
+$lang['software_updates_overall_progress'] = 'Samlet Progress';
+$lang['software_updates_package'] = 'Pakke';
+$lang['software_updates_please_be_patient_detail'] = 'Det kan tage et minut eller to til at opdatere alle repository og opdatere detaljer. Denne information caches for fremtidig brug.';
+$lang['software_updates_please_be_patient'] = 'Vær tålmodig!';
+$lang['software_updates_progress_help'] = 'Dit system er ved at blive opdateret. Denne proces kan tage lidt tid, så vær tålmodig!';
+$lang['software_updates_progress'] = 'Fremskridt';
+$lang['software_updates_recent_software_activity'] = 'Seneste Software Activity';
+$lang['software_updates_repository'] = 'Repository';
+$lang['software_updates_return_to_overview'] = 'Retur til oversigt';
+$lang['software_updates_software_update_in_progress'] = 'Software opdateringen er i gang. Kom tilbage senere.';
+$lang['software_updates_software_updates_busy'] = 'Softwareopdateringer Optaget';
+$lang['software_updates_software_updates'] = 'Software opdateringer';
+$lang['software_updates_software_updates_system_busy'] = 'Softwareopdateringer system er optaget.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'System er up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'De seneste softwareopdateringer er installeret.';
+$lang['software_updates_type'] = 'Type';
+$lang['software_updates_update_all'] = 'Opdater alle';
+$lang['software_updates_updates_complete'] = 'Softwareopdateringer Complete';
+$lang['software_updates_updates'] = 'opdateringer';
+$lang['software_updates_upgrade'] = 'Upgrade';
+$lang['software_updates_wizard_description'] = 'Nu hvor dit netværk er konfigureret, er vigtige opdateringer til kernen systemet nu installeret.';
+$lang['software_updates_wizard_latest_necessary'] = 'Husk at opdatere dit system software, før du fortsætter.';
