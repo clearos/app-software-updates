a186a24a48cb22c055324bc9e48b4ef72396f75d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:46:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'навсозиҳои иловагӣ';
+$lang['software_updates_app_description'] = 'Дар барнома нармафзор Updates навсозиҳои барои ҷузъҳои системаи оператсионии аслї таъмин менамояд.';
+$lang['software_updates_app_name'] = 'Барнома Шарҳҳо';
+$lang['software_updates_automatic_updates'] = 'Updates худкори';
+$lang['software_updates_available_updates'] = 'дастрас Шарҳҳо';
+$lang['software_updates_busy_message'] = 'Боз як истифодабаранда ё хидмат аст, айни замон бо истифода аз системаи навсозиҳои нармафзор. Вақте, ки раванди пурра аст, ин барнома боз дастрас мегардад.';
+$lang['software_updates_current_progress'] = 'Пешравӣ ҷорӣ';
+$lang['software_updates_details'] = 'Cохта шуд';
+$lang['software_updates_install_progress'] = 'Install равӣ';
+$lang['software_updates_loading_updates_message'] = 'Боргузории рӯйхати навсозиҳои нармафзор, лутфан сабр кун.';
+$lang['software_updates_no_data_available'] = 'Не маълумоти мавҷуда.';
+$lang['software_updates_overall_progress'] = 'Пешравӣ умумӣ';
+$lang['software_updates_package'] = 'бастаи';
+$lang['software_updates_please_be_patient_detail'] = 'Он метавонад як ё ду дақиқа барои навсозии тамоми анбори ва навсозии тафсилоти мегирад. Ин маълумот барои истифодаи оянда фарохонӣ шудаанд.';
+$lang['software_updates_please_be_patient'] = 'Лутфан, сабр кун!';
+$lang['software_updates_progress_help'] = 'Системаи Шумо дар ҳоли ҳозир нав карда мешавад. Ин раванд метавонад баъзе вақт даркор аст, то лутфан сабр кун!';
+$lang['software_updates_progress'] = 'пешравӣ';
+$lang['software_updates_recent_software_activity'] = 'Охирин Фаъолияти Software';
+$lang['software_updates_repository'] = 'Repository';
+$lang['software_updates_return_to_overview'] = 'Бозгашт ба шарҳи';
+$lang['software_updates_software_update_in_progress'] = 'Навсозии нармафзор дар пешрафти аст. Лутфан, баргашта дертар.';
+$lang['software_updates_software_updates_busy'] = 'Барнома Шарҳҳо Машғул';
+$lang['software_updates_software_updates'] = 'Барнома Шарҳҳо';
+$lang['software_updates_software_updates_system_busy'] = 'Системаи навсозиҳои нармафзор банд аст.';
+$lang['software_updates_software_up_to_date'] = 'Software То-ба-рӯзи';
+$lang['software_updates_system_is_up_to_date'] = 'Низоми аст, то-ба-санаи.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Дар охирин навсозиҳои нармафзор насб шудаанд.';
+$lang['software_updates_type'] = 'Намуди';
+$lang['software_updates_update_all'] = 'навсозии Ҳамаи';
+$lang['software_updates_updates_complete'] = 'Барнома Шарҳҳо пурра';
+$lang['software_updates_updates'] = 'Шарҳҳо';
+$lang['software_updates_upgrade'] = 'навсозӣ';
+$lang['software_updates_wizard_description'] = 'Акнун, ки шабакаи шумо танзим шуда бошад, навсозиҳои муҳим ба системаи аслӣ ҳоло насб.';
+$lang['software_updates_wizard_latest_necessary'] = 'Лутфан, нармафзори системаи шумо пеш аз давом навсозӣ намояд.';
