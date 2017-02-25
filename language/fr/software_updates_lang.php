cc02169d608d4611b21ec82894d7246b60246805
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:39:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'mises à jour supplémentaires';
+$lang['software_updates_app_description'] = 'L&#39;application fournit des mises à jour logicielles mises à jour pour les composants du système d&#39;exploitation sous-jacent.';
+$lang['software_updates_app_name'] = 'Mises à jour de logiciel';
+$lang['software_updates_automatic_updates'] = 'Mises à jour automatiques';
+$lang['software_updates_available_updates'] = 'Mises à jour disponibles';
+$lang['software_updates_busy_message'] = 'Un autre utilisateur ou service utilise actuellement le système de mises à jour logicielles. Lorsque le processus est terminé, cette application deviendra disponible à nouveau.';
+$lang['software_updates_current_progress'] = 'Progrès en cours';
+$lang['software_updates_details'] = 'Détails';
+$lang['software_updates_install_progress'] = 'progrès de l&#39;installation';
+$lang['software_updates_loading_updates_message'] = 'Chargement liste des mises à jour logicielles, s&#39;il vous plaît soyez patient.';
+$lang['software_updates_no_data_available'] = 'Pas de données disponibles.';
+$lang['software_updates_overall_progress'] = 'Les progrès d&#39;ensemble';
+$lang['software_updates_package'] = 'Paquet';
+$lang['software_updates_please_be_patient_detail'] = 'Il peut prendre une minute ou deux pour mettre à jour tous les référentiels et mettre à jour les détails. Ces informations sont mises en cache pour une utilisation future.';
+$lang['software_updates_please_be_patient'] = 'S&#39;il vous plaît être patient!';
+$lang['software_updates_progress_help'] = 'Votre système est actuellement mis à jour. Ce processus peut prendre un certain temps, donc s&#39;il vous plaît soyez patient!';
+$lang['software_updates_progress'] = 'Le progrès';
+$lang['software_updates_recent_software_activity'] = 'Software Activité récente';
+$lang['software_updates_repository'] = 'Dépôt';
+$lang['software_updates_return_to_overview'] = 'Retour au sommaire';
+$lang['software_updates_software_update_in_progress'] = 'mise à jour du logiciel est en cours. Merci de revenir plus tard.';
+$lang['software_updates_software_updates_busy'] = 'Mises à jour logicielles Busy';
+$lang['software_updates_software_updates'] = 'Mises à jour de logiciel';
+$lang['software_updates_software_updates_system_busy'] = 'système de mises à jour logicielles est occupé.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-Date';
+$lang['software_updates_system_is_up_to_date'] = 'Le système est mis à jour.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Les dernières mises à jour logicielles sont installées.';
+$lang['software_updates_type'] = 'Type';
+$lang['software_updates_update_all'] = 'Tout mettre à jour';
+$lang['software_updates_updates_complete'] = 'Mises à jour logicielles complètes';
+$lang['software_updates_updates'] = 'Mises à jour';
+$lang['software_updates_upgrade'] = 'Surclassement';
+$lang['software_updates_wizard_description'] = 'Maintenant que votre réseau est configuré, les mises à jour critiques du système de base sont maintenant installés.';
+$lang['software_updates_wizard_latest_necessary'] = 'S&#39;il vous plaît mettre à jour votre logiciel système avant de continuer.';
