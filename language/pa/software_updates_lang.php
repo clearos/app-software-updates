ab883f08c2972b92aa546cc5d487b9dc2143fe7c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:43:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'ਵਾਧੂ ਅੱਪਡੇਟ';
+$lang['software_updates_app_description'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਐਪ ਅਸਲ ਵਿੱਚ ਓਪਰੇਟਿੰਗ ਸਿਸਟਮ ਭਾਗ ਲਈ ਅੱਪਡੇਟ ਦਿੰਦਾ ਹੈ.';
+$lang['software_updates_app_name'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ';
+$lang['software_updates_automatic_updates'] = 'ਆਟੋਮੈਟਿਕ ਅੱਪਡੇਟ';
+$lang['software_updates_available_updates'] = 'ਉਪਲੱਬਧ ਅੱਪਡੇਟ';
+$lang['software_updates_busy_message'] = 'ਹੋਰ ਯੂਜ਼ਰ ਜ ਸੇਵਾ ਮੌਜੂਦਾ ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਸਿਸਟਮ ਵਰਤ ਰਿਹਾ ਹੈ. ਜਦ ਕਾਰਜ ਨੂੰ ਮੁਕੰਮਲ ਹੋ ਗਈ ਹੈ, ਇਸ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਮੁੜ ਉਪਲੱਬਧ ਹੋ ਜਾਵੇਗਾ.';
+$lang['software_updates_current_progress'] = 'ਵਰਤਮਾਨ ਤਰੱਕੀ';
+$lang['software_updates_details'] = 'ਵੇਰਵਾ';
+$lang['software_updates_install_progress'] = 'ਤਰੱਕੀ ਇੰਸਟਾਲ ਕਰੋ';
+$lang['software_updates_loading_updates_message'] = 'ਲੋਡ ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਸੂਚੀ, ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਹੋ.';
+$lang['software_updates_no_data_available'] = 'ਕੋਈ ਡਾਟਾ ਉਪਲੱਬਧ ਹੈ.';
+$lang['software_updates_overall_progress'] = 'ਓਵਰਆਲ ਤਰੱਕੀ';
+$lang['software_updates_package'] = 'ਪੈਕੇਜ';
+$lang['software_updates_please_be_patient_detail'] = 'ਇਹ ਸਭ ਰਿਪੋਜ਼ਟਰੀ ਅਤੇ ਅੱਪਡੇਟ ਵੇਰਵੇ ਨੂੰ ਅੱਪਡੇਟ ਕਰਨ ਲਈ ਇਕ-ਦੋ ਮਿੰਟ ਲੈ ਸਕਦਾ ਹੈ. ਇਹ ਜਾਣਕਾਰੀ ਭਵਿੱਖ ਦੇ ਵਰਤਣ ਲਈ ਕੈਸ਼ ਹੈ.';
+$lang['software_updates_please_be_patient'] = 'ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਹੋ!';
+$lang['software_updates_progress_help'] = 'ਤੁਹਾਡਾ ਸਿਸਟਮ ਇਸ ਵੇਲੇ ਅੱਪਡੇਟ ਕੀਤਾ ਜਾ ਰਿਹਾ ਹੈ. ਇਸ ਕਾਰਵਾਈ ਨੂੰ ਕੁਝ ਟਾਈਮ ਲੱਗ ਸਕਦਾ ਹੈ, ਇਸ ਲਈ ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਨੂੰ ਹੋ!';
+$lang['software_updates_progress'] = 'ਤਰੱਕੀ';
+$lang['software_updates_recent_software_activity'] = 'ਹਾਲੀਆ ਸਾਫਟਵੇਅਰ ਸਰਗਰਮੀ';
+$lang['software_updates_repository'] = 'ਰਿਪੋਜ਼ਟਰੀ';
+$lang['software_updates_return_to_overview'] = 'ਝਲਕ ਵਾਪਸ';
+$lang['software_updates_software_update_in_progress'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਦੀ ਤਰੱਕੀ ਵਿੱਚ ਹੈ. ਕਿਰਪਾ ਕਰਕੇ ਬਾਅਦ ਵਿੱਚ ਵਾਪਸ ਆ.';
+$lang['software_updates_software_updates_busy'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਰੁਝਿਆ';
+$lang['software_updates_software_updates'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ';
+$lang['software_updates_software_updates_system_busy'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਸਿਸਟਮ ਰੁੱਝਿਆ ਹੋਇਆ ਹੈ.';
+$lang['software_updates_software_up_to_date'] = 'ਉੱਪਰ-ਨੂੰ-ਦੀ ਮਿਤੀ ਸਾਫਟਵੇਅਰ';
+$lang['software_updates_system_is_up_to_date'] = 'ਸਿਸਟਮ ਨੂੰ-ਟੂ-ਡੇਟ ਹੈ.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'ਤਾਜ਼ਾ ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਇੰਸਟਾਲ ਕਰ ਰਹੇ ਹੋ.';
+$lang['software_updates_type'] = 'ਦੀ ਕਿਸਮ';
+$lang['software_updates_update_all'] = 'ਸਾਰੇ ਅੱਪਡੇਟ';
+$lang['software_updates_updates_complete'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਮੁਕੰਮਲ';
+$lang['software_updates_updates'] = 'ਅੱਪਡੇਟ';
+$lang['software_updates_upgrade'] = 'ਅੱਪਗਰੇਡ';
+$lang['software_updates_wizard_description'] = 'ਹੁਣ ਹੈ, ਜੋ ਕਿ ਤੁਹਾਡੇ ਨੈੱਟਵਰਕ ਸੰਰਚਿਤ ਕੀਤਾ ਹੈ, ਕੋਰ ਸਿਸਟਮ ਨੂੰ ਨਾਜ਼ੁਕ ਅੱਪਡੇਟ ਹੁਣ ਇੰਸਟਾਲ ਹਨ.';
+$lang['software_updates_wizard_latest_necessary'] = 'ਜਾਰੀ ਅੱਗੇ ਤੁਹਾਡੇ ਸਿਸਟਮ ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਕਰੋ.';
