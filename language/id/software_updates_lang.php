661fde7c792611b62cbd55a610b743eb2e29e38e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:40:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'update tambahan';
+$lang['software_updates_app_description'] = 'Software Update aplikasi menyediakan update untuk komponen sistem operasi yang mendasarinya.';
+$lang['software_updates_app_name'] = 'Pembaruan perangkat lunak';
+$lang['software_updates_automatic_updates'] = 'Automatic Updates';
+$lang['software_updates_available_updates'] = 'tersedia Updates';
+$lang['software_updates_busy_message'] = 'Pengguna lain atau layanan saat ini menggunakan sistem pembaruan perangkat lunak. Ketika proses selesai, aplikasi ini akan tersedia lagi.';
+$lang['software_updates_current_progress'] = 'Kemajuan saat ini';
+$lang['software_updates_details'] = 'rincian';
+$lang['software_updates_install_progress'] = 'install Kemajuan';
+$lang['software_updates_loading_updates_message'] = 'Memuat daftar pembaruan perangkat lunak, harap bersabar.';
+$lang['software_updates_no_data_available'] = 'Tidak ada data yang tersedia.';
+$lang['software_updates_overall_progress'] = 'Kemajuan secara keseluruhan';
+$lang['software_updates_package'] = 'Paket';
+$lang['software_updates_please_be_patient_detail'] = 'Ini dapat mengambil satu atau dua menit untuk memperbarui semua repositori dan pembaruan detail. Informasi ini cache untuk penggunaan masa depan.';
+$lang['software_updates_please_be_patient'] = 'Harap bersabar!';
+$lang['software_updates_progress_help'] = 'sistem Anda saat ini sedang diperbarui. Proses ini dapat memakan waktu, jadi harap bersabar!';
+$lang['software_updates_progress'] = 'Kemajuan';
+$lang['software_updates_recent_software_activity'] = 'Software Kegiatan Terbaru';
+$lang['software_updates_repository'] = 'Gudang';
+$lang['software_updates_return_to_overview'] = 'Kembali ke Ikhtisar';
+$lang['software_updates_software_update_in_progress'] = 'pembaruan perangkat lunak berlangsung. Silahkan kembali lagi nanti.';
+$lang['software_updates_software_updates_busy'] = 'Software Update Sibuk';
+$lang['software_updates_software_updates'] = 'Pembaruan perangkat lunak';
+$lang['software_updates_software_updates_system_busy'] = 'Sistem pembaruan perangkat lunak sibuk.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'Sistem up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Pembaruan perangkat lunak terbaru diinstal.';
+$lang['software_updates_type'] = 'Mengetik';
+$lang['software_updates_update_all'] = 'Perbarui Semua';
+$lang['software_updates_updates_complete'] = 'Software Update Lengkap';
+$lang['software_updates_updates'] = 'update';
+$lang['software_updates_upgrade'] = 'Meningkatkan';
+$lang['software_updates_wizard_description'] = 'Sekarang jaringan Anda dikonfigurasi, update penting untuk sistem inti sekarang diinstal.';
+$lang['software_updates_wizard_latest_necessary'] = 'Perbarui perangkat lunak sistem Anda sebelum melanjutkan.';
