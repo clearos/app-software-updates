10b93b2d76a74d79642858928d6977d24933a848
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:42:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'Papildomos atnaujinimai';
+$lang['software_updates_app_description'] = 'Programinės įrangos atnaujinimai programa suteikia atnaujinimus operacinės sistemos komponentų.';
+$lang['software_updates_app_name'] = 'programinė įranga atnaujinimai';
+$lang['software_updates_automatic_updates'] = 'Automatiniai atnaujinimai';
+$lang['software_updates_available_updates'] = 'Turimas Atnaujinimai';
+$lang['software_updates_busy_message'] = 'Kitas vartotojas ar paslauga šiuo metu naudojasi programinės įrangos atnaujinimus sistemą. Kai procesas bus baigtas, ši programa bus prieinama dar kartą.';
+$lang['software_updates_current_progress'] = 'Dabartinis pažanga';
+$lang['software_updates_details'] = 'detalės';
+$lang['software_updates_install_progress'] = 'Įdiekite Progress';
+$lang['software_updates_loading_updates_message'] = 'Kraunasi programinės įrangos atnaujinimus, sąrašas, būkite kantrūs.';
+$lang['software_updates_no_data_available'] = 'Nėra duomenų.';
+$lang['software_updates_overall_progress'] = 'Bendras progresas';
+$lang['software_updates_package'] = 'paketas';
+$lang['software_updates_please_be_patient_detail'] = 'Tai gali užtrukti minutę ar dvi atnaujinti visas saugyklos ir atnaujinti duomenis. Ši informacija yra išsaugota vėlesniam naudojimui.';
+$lang['software_updates_please_be_patient'] = 'Būkite kantrūs!';
+$lang['software_updates_progress_help'] = 'Jūsų sistema šiuo metu yra atnaujinama. Šis procesas gali užtrukti, todėl būkite kantrūs!';
+$lang['software_updates_progress'] = 'Progresas';
+$lang['software_updates_recent_software_activity'] = 'Naujausios Programinė įranga Veiklos';
+$lang['software_updates_repository'] = 'saugykla';
+$lang['software_updates_return_to_overview'] = 'Grįžti į peržiūrą';
+$lang['software_updates_software_update_in_progress'] = 'Programinės įrangos naujinimas yra pažangos. Prašome užsukti vėliau.';
+$lang['software_updates_software_updates_busy'] = 'Programinė įranga Atnaujinimai Užimta';
+$lang['software_updates_software_updates'] = 'programinė įranga atnaujinimai';
+$lang['software_updates_software_updates_system_busy'] = 'Programinės įrangos atnaujinimai sistema yra užimtas.';
+$lang['software_updates_software_up_to_date'] = 'Programinė įranga up-to-data';
+$lang['software_updates_system_is_up_to_date'] = 'Sistema yra up-to-data.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Naujos programinės įrangos atnaujinimai yra įdiegta.';
+$lang['software_updates_type'] = 'tipas';
+$lang['software_updates_update_all'] = 'Atnaujinti viską';
+$lang['software_updates_updates_complete'] = 'Programinė įranga Atnaujinimai Užbaigti';
+$lang['software_updates_updates'] = 'Atnaujinimai';
+$lang['software_updates_upgrade'] = 'Patobulinti';
+$lang['software_updates_wizard_description'] = 'Dabar, kad jūsų tinklas yra sukonfigūruotas, dabar įdiegta kritiniai atnaujinimai pagrindinei sistemai.';
+$lang['software_updates_wizard_latest_necessary'] = 'Atnaujinkite savo sistemos programinę įrangą prieš tęsiant.';
