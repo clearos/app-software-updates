9a75cb75e94eb721fe23e06d37553cce925eb738
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:47:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'נאָך ופּדאַטעס';
+$lang['software_updates_app_description'] = 'די ווייכווארג ופּדאַטעס אַפּ גיט ופּדאַטעס פֿאַר די אַנדערלייינג אָפּערייטינג סיסטעם קאַמפּאָונאַנץ.';
+$lang['software_updates_app_name'] = 'ווייכווארג ופּדאַטעס';
+$lang['software_updates_automatic_updates'] = 'אָטאַמאַטיק ופּדאַטעס';
+$lang['software_updates_available_updates'] = 'בנימצא ופּדאַטעס';
+$lang['software_updates_busy_message'] = 'אן אנדער באַניצער אָדער דינסט איז איצט ניצן די ווייכווארג ופּדאַטעס סיסטעם. ווען דער פּראָצעס איז גאַנץ, דעם אַפּ וועט ווערן פאַראַנען ווידער.';
+$lang['software_updates_current_progress'] = 'קראַנט פּראָגרעס';
+$lang['software_updates_details'] = 'דעטאַילס';
+$lang['software_updates_install_progress'] = 'ינסטאַלירן פּראַגרעס';
+$lang['software_updates_loading_updates_message'] = 'לאָודינג ווייכווארג ופּדאַטעס רשימה, ביטע זייַן פּאַציענט.';
+$lang['software_updates_no_data_available'] = 'קיין דאַטן בנימצא.';
+$lang['software_updates_overall_progress'] = 'קוילעלדיק פּראָגרעס';
+$lang['software_updates_package'] = 'פּעקל';
+$lang['software_updates_please_be_patient_detail'] = 'עס קענען נעמען אַ מינוט אָדער צוויי צו דערהייַנטיקן אַלע די ריפּאַזאַטאָרי און דערהייַנטיקן דעטאַילס. דעם אינפֿאָרמאַציע איז קאַשט פֿאַר צוקונפֿט נוצן.';
+$lang['software_updates_please_be_patient'] = 'ביטע זיין פּאַציענט!';
+$lang['software_updates_progress_help'] = 'דיין סיסטעם איז איצט ווייל ופּדאַטעד. דעם פּראָצעס קענען נעמען עטלעכע מאָל, אַזוי ביטע זיין פּאַציענט!';
+$lang['software_updates_progress'] = 'פּראָגרעס';
+$lang['software_updates_recent_software_activity'] = 'פריש ווייכווארג אַקטיוויטי';
+$lang['software_updates_repository'] = 'ריפּאַזאַטאָרי';
+$lang['software_updates_return_to_overview'] = 'צוריקקומען צו איבערבליק';
+$lang['software_updates_software_update_in_progress'] = 'ווייכווארג דערהייַנטיקן איז אין פּראָגרעס. ביטע קומען צוריק שפּעטער.';
+$lang['software_updates_software_updates_busy'] = 'ווייכווארג ופּדאַטעס Busy';
+$lang['software_updates_software_updates'] = 'ווייכווארג ופּדאַטעס';
+$lang['software_updates_software_updates_system_busy'] = 'ווייכווארג ופּדאַטעס סיסטעם איז פאַרנומען.';
+$lang['software_updates_software_up_to_date'] = 'ווייכווארג אַרויף-צו-טאָג';
+$lang['software_updates_system_is_up_to_date'] = 'סיסטעם איז אַרויף-צו-דאַטע.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'די לעצט ווייכווארג ופּדאַטעס זענען אינסטאַלירן.';
+$lang['software_updates_type'] = 'טיפּ';
+$lang['software_updates_update_all'] = 'אַפּדייט אַלע';
+$lang['software_updates_updates_complete'] = 'ווייכווארג ופּדאַטעס גאַנץ';
+$lang['software_updates_updates'] = 'ופּדאַטעס';
+$lang['software_updates_upgrade'] = 'ופּגראַדע';
+$lang['software_updates_wizard_description'] = 'איצט אַז אייער נעץ איז קאָנפיגורעד, קריטיש אַפּדייץ צו די האַרץ סיסטעם זענען איצט אינסטאַלירן.';
+$lang['software_updates_wizard_latest_necessary'] = 'ביטע דערהייַנטיקן דיין סיסטעם ווייכווארג איידער קאַנטיניוינג.';
