d8539d34cec65653c4d8e34fd0383bbd5b76d6f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:45:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'diapdeite tsa tlatsetso';
+$lang['software_updates_app_description'] = 'The app e Software Updates fana ka diapdeite bakeng sa bothata bo itseng &#39;dikarolo tshebetso ya ho sebetsa.';
+$lang['software_updates_app_name'] = 'Software Updates';
+$lang['software_updates_automatic_updates'] = 'jarolla ka ho iketsa Updates';
+$lang['software_updates_available_updates'] = 'fumaneha Updates';
+$lang['software_updates_busy_message'] = 'user want-meet.ru kapa tshebeletso e nngwe hona joale sebelisa diapdeite tsa software tsamaiso. Ha tshebetso ke ka ho feletseng, app e sena e tla ba fumaneha hape.';
+$lang['software_updates_current_progress'] = 'Current Hatele Pele';
+$lang['software_updates_details'] = 'Details';
+$lang['software_updates_install_progress'] = 'kenya Hatele Pele';
+$lang['software_updates_loading_updates_message'] = 'Loading diapdeite tsa software le siyo lenaneng, ka kōpo ba le mamello.';
+$lang['software_updates_no_data_available'] = 'No ya data fumaneha.';
+$lang['software_updates_overall_progress'] = 'Ka kakaretso Hatele Pele';
+$lang['software_updates_package'] = 'sephutheloana';
+$lang['software_updates_please_be_patient_detail'] = 'Ho ka nka motsotso kapa tse peli ho ntjhafatsa ea polokelo ebe o apdeita makolopetso &#39;ohle. Tlhahisoleseding ena e Cached bakeng sa ho sebelisoa ka moso.';
+$lang['software_updates_please_be_patient'] = 'Ka kōpo ba le mamello!';
+$lang['software_updates_progress_help'] = 'tsamaiso ea hao ea hona joale a ntse a ntjhafatswa. Tshebetso ena e ka nka nako e itseng, kahoo ka kōpo ba le mamello!';
+$lang['software_updates_progress'] = 'tsoelo-pele ea';
+$lang['software_updates_recent_software_activity'] = 'Recent Software Activity';
+$lang['software_updates_repository'] = 'polokelo';
+$lang['software_updates_return_to_overview'] = 'Khutlela ho Overview';
+$lang['software_updates_software_update_in_progress'] = 'Apdeite ya software e tsoela pele. Ka kōpo khutla hamorao.';
+$lang['software_updates_software_updates_busy'] = 'Software Updates Phathahane';
+$lang['software_updates_software_updates'] = 'Software Updates';
+$lang['software_updates_software_updates_system_busy'] = 'Software diapdeite tsamaiso phathahane.';
+$lang['software_updates_software_up_to_date'] = 'Software Up-rao';
+$lang['software_updates_system_is_up_to_date'] = 'System ke ba morao-letsatsi.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'The diapdeite software ya moraorao di hlomamisa.';
+$lang['software_updates_type'] = 'Type';
+$lang['software_updates_update_all'] = 'Update All';
+$lang['software_updates_updates_complete'] = 'Software Updates Complete';
+$lang['software_updates_updates'] = 'Updates';
+$lang['software_updates_upgrade'] = 'apkreite';
+$lang['software_updates_wizard_description'] = 'Kaha joale netweke ya hao e di kentswe, diapdeite tse mahlonoko tseo ho tsamaiso ea mantlha hona joale hlomamisa.';
+$lang['software_updates_wizard_latest_necessary'] = 'Ka kōpo ntjhafatsa tsamaiso Software hao ka pel&#39;a tsoela pele.';
