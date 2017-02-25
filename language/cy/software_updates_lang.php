b488ae9df06592f35395eddfe9007ac9a0b65442
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:47:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'diweddariadau ychwanegol';
+$lang['software_updates_app_description'] = 'Mae&#39;r app Diweddariadau Meddalwedd yn darparu diweddariadau ar gyfer y cydrannau system weithredu sylfaenol.';
+$lang['software_updates_app_name'] = 'Diweddariadau meddalwedd';
+$lang['software_updates_automatic_updates'] = 'Awtomatig Updates';
+$lang['software_updates_available_updates'] = 'Diweddariadau ar gael';
+$lang['software_updates_busy_message'] = 'defnyddiwr neu wasanaeth arall o bryd yn defnyddio&#39;r system diweddariadau meddalwedd. Pan fydd y broses wedi&#39;i chwblhau, bydd hyn app yn dod ar gael eto.';
+$lang['software_updates_current_progress'] = 'Cynnydd ar hyn o bryd';
+$lang['software_updates_details'] = 'manylion';
+$lang['software_updates_install_progress'] = 'Gosod Cynnydd';
+$lang['software_updates_loading_updates_message'] = 'Llwytho rhestr diweddariadau meddalwedd, byddwch yn amyneddgar.';
+$lang['software_updates_no_data_available'] = 'Dim data ar gael.';
+$lang['software_updates_overall_progress'] = 'Cynnydd cyffredinol';
+$lang['software_updates_package'] = 'pecyn';
+$lang['software_updates_please_be_patient_detail'] = 'Gall gymryd munud neu ddau i ddiweddaru holl ystorfa a diweddaru manylion. Mae&#39;r wybodaeth hon yn cael ei cached ar gyfer defnydd yn y dyfodol.';
+$lang['software_updates_please_be_patient'] = 'Byddwch yn amyneddgar!';
+$lang['software_updates_progress_help'] = 'Mae eich system yn cael ei ddiweddaru ar hyn o bryd. Gall y broses gymryd peth amser, felly byddwch yn amyneddgar!';
+$lang['software_updates_progress'] = 'cynnydd';
+$lang['software_updates_recent_software_activity'] = 'Gweithgaredd Meddalwedd Diweddar';
+$lang['software_updates_repository'] = 'ystorfa';
+$lang['software_updates_return_to_overview'] = 'Dychwelyd i&#39;r Trosolwg';
+$lang['software_updates_software_update_in_progress'] = 'Diweddariad Meddalwedd ar y gweill. Os gwelwch yn dda dod yn ôl yn ddiweddarach.';
+$lang['software_updates_software_updates_busy'] = 'Diweddariadau meddalwedd Prysur';
+$lang['software_updates_software_updates'] = 'Diweddariadau meddalwedd';
+$lang['software_updates_software_updates_system_busy'] = 'system diweddariadau meddalwedd yn brysur.';
+$lang['software_updates_software_up_to_date'] = 'Meddalwedd Hyd cyfredol';
+$lang['software_updates_system_is_up_to_date'] = 'System yw hyd yn gyfredol.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Mae&#39;r diweddariadau meddalwedd diweddaraf yn cael eu gosod.';
+$lang['software_updates_type'] = 'math';
+$lang['software_updates_update_all'] = 'Diweddariad Gyfan';
+$lang['software_updates_updates_complete'] = 'Diweddariadau meddalwedd Cwblhau';
+$lang['software_updates_updates'] = 'diweddariadau';
+$lang['software_updates_upgrade'] = 'uwchraddio';
+$lang['software_updates_wizard_description'] = 'Nawr bod eich rhwydwaith yn cael ei ffurfweddu, diweddariadau critigol i&#39;r system graidd bellach yn cael eu gosod.';
+$lang['software_updates_wizard_latest_necessary'] = 'Os gwelwch yn dda ddiweddaru eich meddalwedd system cyn parhau.';
