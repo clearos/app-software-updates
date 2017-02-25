1be1d804ad7aba0409a25c7264b80f55f1fa8b4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:42:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'അധിക അപ്ഡേറ്റുകൾ';
+$lang['software_updates_app_description'] = 'സോഫ്റ്റ്വെയർ പരിഷ്കരണങ്ങൾ അപ്ലിക്കേഷൻ ഓപ്പറേറ്റിങ് സിസ്റ്റത്തിന്റെ ഘടകങ്ങൾ അപ്ഡേറ്റുകൾ നൽകുന്നു.';
+$lang['software_updates_app_name'] = 'സോഫ്റ്റ്വെയർ പരിഷ്കരണങ്ങൾ';
+$lang['software_updates_automatic_updates'] = 'ഓട്ടോമാറ്റിക് അപ്ഡേറ്റുകൾ';
+$lang['software_updates_available_updates'] = 'അപ്ഡേറ്റുകൾ ലഭ്യമാണ്';
+$lang['software_updates_busy_message'] = 'മറ്റൊരു ഉപയോക്താവ് അല്ലെങ്കിൽ സേവനം നിലവിൽ സോഫ്റ്റ്വെയർ അപ്ഡേറ്റുകൾ സിസ്റ്റം ഉപയോഗിക്കുന്നു. പ്രക്രിയ പൂർത്തിയാകുമ്പോൾ, ഈ അപ്ലിക്കേഷൻ വീണ്ടും ലഭ്യമാകും.';
+$lang['software_updates_current_progress'] = 'ഇപ്പോഴത്തെ പുരോഗതി';
+$lang['software_updates_details'] = 'വിവരങ്ങൾ';
+$lang['software_updates_install_progress'] = 'പുരോഗതി ഇൻസ്റ്റാൾ';
+$lang['software_updates_loading_updates_message'] = 'ലോഡുചെയ്യുന്നു സോഫ്റ്റ്വെയർ അപ്ഡേറ്റുകൾ ലിസ്റ്റ്, ദയവായി ക്ഷമയോടെ.';
+$lang['software_updates_no_data_available'] = 'വിവരമൊന്നും ലഭ്യമല്ല.';
+$lang['software_updates_overall_progress'] = 'മൊത്തത്തിലുള്ള പുരോഗതി';
+$lang['software_updates_package'] = 'പാക്കേജ്';
+$lang['software_updates_please_be_patient_detail'] = 'ഇത് എല്ലാ ശേഖരത്തിലും അപ്ഡേറ്റ് വിശദാംശങ്ങൾ പുതുക്കുന്നതിനായി ഒന്നോ രണ്ടോ മിനിട്ട് എടുക്കാം. ഈ വിവരം ഭാവിയിലെ ഉപയോഗത്തിനായി കാഷെ ചെയ്തു.';
+$lang['software_updates_please_be_patient'] = 'ദയവായി ക്ഷമിക്കുക!';
+$lang['software_updates_progress_help'] = 'നിങ്ങളുടെ സിസ്റ്റം നിലവിൽ അപ്ഡേറ്റ് ചെയ്തുകൊണ്ടിരിക്കുന്നു. ഈ പ്രക്രിയ കുറച്ച് സമയമെടുക്കും, ദയവായി ക്ഷമയോടെ!';
+$lang['software_updates_progress'] = 'പുരോഗതി';
+$lang['software_updates_recent_software_activity'] = 'സമീപകാല സോഫ്റ്റ്വെയർ പ്രവർത്തനം';
+$lang['software_updates_repository'] = 'സംഭരണിയാണ്';
+$lang['software_updates_return_to_overview'] = 'പരിശോധന മടങ്ങുക';
+$lang['software_updates_software_update_in_progress'] = 'സോഫ്റ്റ്വെയർ അപ്ഡേറ്റ് പുരോഗതിയിലാണ്. പിന്നീട് വീണ്ടും വരിക.';
+$lang['software_updates_software_updates_busy'] = 'സോഫ്റ്റ്വെയർ പരിഷ്കരണങ്ങൾ തിരക്കിലാണ്';
+$lang['software_updates_software_updates'] = 'സോഫ്റ്റ്വെയർ പരിഷ്കരണങ്ങൾ';
+$lang['software_updates_software_updates_system_busy'] = 'സോഫ്റ്റ്വെയർ അപ്ഡേറ്റുകൾ സിസ്റ്റം തിരക്കിലാണ്.';
+$lang['software_updates_software_up_to_date'] = 'സോഫ്റ്റ്വെയർ കാലികമായി';
+$lang['software_updates_system_is_up_to_date'] = 'സിസ്റ്റം അപ്-റ്റു-ഡേറ്റ് ആണ്.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'പുതിയ സോഫ്റ്റ്വെയർ അപ്ഡേറ്റുകൾ ഇൻസ്റ്റാൾ.';
+$lang['software_updates_type'] = 'ടൈപ്പ് ചെയ്യുക';
+$lang['software_updates_update_all'] = 'എല്ലാം അപ്ഡേറ്റുചെയ്യുക';
+$lang['software_updates_updates_complete'] = 'പൂർത്തിയാക്കുക സോഫ്റ്റ്വെയർ പരിഷ്കരണങ്ങൾ';
+$lang['software_updates_updates'] = 'അപ്ഡേറ്റുകൾ';
+$lang['software_updates_upgrade'] = 'നവീകരിക്കുക';
+$lang['software_updates_wizard_description'] = 'ഇപ്പോൾ നിങ്ങളുടെ നെറ്റ്വർക്ക് കോൺഫിഗർ കോർ സിസ്റ്റത്തിലേക്ക് നിർണ്ണായക അപ്ഡേറ്റുകൾ ഇപ്പോൾ ഇൻസ്റ്റാൾ.';
+$lang['software_updates_wizard_latest_necessary'] = 'തുടരുന്നതിന് മുമ്പ് നിങ്ങളുടെ സിസ്റ്റം സോഫ്റ്റ്വെയർ അപ്ഡേറ്റ് ചെയ്യുക.';
