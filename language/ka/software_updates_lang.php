7ccb59e807400ffe961b4f4ceaa28a702cc3bb6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:41:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'დამატებითი განახლებები';
+$lang['software_updates_app_description'] = 'პროგრამული სიახლეები app უზრუნველყოფს განახლება ფუძემდებლური ოპერაციული სისტემის კომპონენტები.';
+$lang['software_updates_app_name'] = 'პროგრამული სიახლეები';
+$lang['software_updates_automatic_updates'] = 'ავტომატური განახლება';
+$lang['software_updates_available_updates'] = 'არსებული განახლების';
+$lang['software_updates_busy_message'] = 'სხვა მომხმარებლის ან მომსახურების ამჟამად გამოყენებით პროგრამული განახლება სისტემა. როდესაც პროცესი დასრულებულია, ამ app ხელმისაწვდომი გახდება ერთხელ.';
+$lang['software_updates_current_progress'] = 'აქტუალური პროგრესი';
+$lang['software_updates_details'] = 'დაწვრილებით';
+$lang['software_updates_install_progress'] = 'Install პროგრესი';
+$lang['software_updates_loading_updates_message'] = 'იტვირთება პროგრამული უზრუნველყოფის განახლებების სია, გთხოვთ მოთმინება.';
+$lang['software_updates_no_data_available'] = 'მონაცემები არ არის.';
+$lang['software_updates_overall_progress'] = 'საერთო პროგრესი';
+$lang['software_updates_package'] = 'პაკეტი';
+$lang['software_updates_please_be_patient_detail'] = 'მას შეუძლია მიიღოს ერთი წუთით ან ორი განახლება ყველა საცავი და განახლება. ეს ინფორმაცია ქეშირებული მომავალი გამოყენება.';
+$lang['software_updates_please_be_patient'] = 'გთხოვთ მოთმინება!';
+$lang['software_updates_progress_help'] = 'თქვენი სისტემის, ამჟამად მიმდინარეობს განახლება. ეს პროცესი შეიძლება გარკვეული დრო, ასე რომ გთხოვთ მოთმინება!';
+$lang['software_updates_progress'] = 'პროგრესი';
+$lang['software_updates_recent_software_activity'] = 'ბოლო Software აქტიურობა';
+$lang['software_updates_repository'] = 'საცავი';
+$lang['software_updates_return_to_overview'] = 'დაბრუნება მიმოხილვა';
+$lang['software_updates_software_update_in_progress'] = 'პროგრამული განახლება მიმდინარეობს. გთხოვთ, მოგვიანებით.';
+$lang['software_updates_software_updates_busy'] = 'პროგრამული სიახლეები Busy';
+$lang['software_updates_software_updates'] = 'პროგრამული სიახლეები';
+$lang['software_updates_software_updates_system_busy'] = 'პროგრამული უზრუნველყოფის განახლება სისტემა არის დაკავებული.';
+$lang['software_updates_software_up_to_date'] = 'პროგრამული Up-to-date';
+$lang['software_updates_system_is_up_to_date'] = 'სისტემა არის up-to-date.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'უახლესი პროგრამული განახლება დაყენებული.';
+$lang['software_updates_type'] = 'ტიპი';
+$lang['software_updates_update_all'] = 'ყველაფრის განახლება';
+$lang['software_updates_updates_complete'] = 'პროგრამული სიახლეები სრული';
+$lang['software_updates_updates'] = 'ჩემ';
+$lang['software_updates_upgrade'] = 'განახლება';
+$lang['software_updates_wizard_description'] = 'ახლა, თქვენი ქსელის კონფიგურაცია, კრიტიკული განახლება ძირითადი სისტემის ახლა დაყენებული.';
+$lang['software_updates_wizard_latest_necessary'] = 'გთხოვთ განაახლოთ თქვენი სისტემის პროგრამული გაგრძელებამდე.';
