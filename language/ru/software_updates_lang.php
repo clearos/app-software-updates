252f287f3c2096016cadb56a00afa3d4895f7e44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:44:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'дополнительные обновления';
+$lang['software_updates_app_description'] = 'Приложение Обновление программного обеспечения предоставляет обновления для основных компонентов операционной системы.';
+$lang['software_updates_app_name'] = 'Обновления программного обеспечения';
+$lang['software_updates_automatic_updates'] = 'Автоматические обновления';
+$lang['software_updates_available_updates'] = 'Доступные обновления';
+$lang['software_updates_busy_message'] = 'Другой пользователь или услуга в настоящее время использует систему обновлений программного обеспечения. Когда процесс будет завершен, это приложение станет доступным снова.';
+$lang['software_updates_current_progress'] = 'Текущий прогресс';
+$lang['software_updates_details'] = 'Детали';
+$lang['software_updates_install_progress'] = 'Установить Прогресс';
+$lang['software_updates_loading_updates_message'] = 'Загрузка списка обновлений программного обеспечения, пожалуйста, будьте терпеливы.';
+$lang['software_updates_no_data_available'] = 'Данные недоступны.';
+$lang['software_updates_overall_progress'] = 'Общий прогресс';
+$lang['software_updates_package'] = 'пакет';
+$lang['software_updates_please_be_patient_detail'] = 'Это может занять минуту или две, чтобы обновить все хранилище и обновить данные. Эта информация кэшируется для использования в будущем.';
+$lang['software_updates_please_be_patient'] = 'Пожалуйста, будьте терпеливы!';
+$lang['software_updates_progress_help'] = 'Ваша система в настоящее время обновляется. Этот процесс может занять некоторое время, поэтому, пожалуйста, будьте терпеливы!';
+$lang['software_updates_progress'] = 'Прогресс';
+$lang['software_updates_recent_software_activity'] = 'Последние версии программного обеспечения активность';
+$lang['software_updates_repository'] = 'вместилище';
+$lang['software_updates_return_to_overview'] = 'Вернуться к обзору';
+$lang['software_updates_software_update_in_progress'] = 'Обновление программного обеспечения в процессе. Пожалуйста, вернитесь позже.';
+$lang['software_updates_software_updates_busy'] = 'Обновления программного обеспечения занятости';
+$lang['software_updates_software_updates'] = 'Обновления программного обеспечения';
+$lang['software_updates_software_updates_system_busy'] = 'Система обновлений программного обеспечения занята.';
+$lang['software_updates_software_up_to_date'] = 'Программное обеспечение до современных';
+$lang['software_updates_system_is_up_to_date'] = 'Система до современных.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'Последние обновления программного обеспечения устанавливаются.';
+$lang['software_updates_type'] = 'Тип';
+$lang['software_updates_update_all'] = 'Обновить все';
+$lang['software_updates_updates_complete'] = 'Обновления программного обеспечения Полная';
+$lang['software_updates_updates'] = 'Обновления';
+$lang['software_updates_upgrade'] = 'Обновить';
+$lang['software_updates_wizard_description'] = 'Теперь, когда сеть настроена, критические обновления ядра системы теперь установлены.';
+$lang['software_updates_wizard_latest_necessary'] = 'Необходимо обновить системное программное обеспечение, прежде чем продолжить.';
