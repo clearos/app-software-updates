4739d6bb8e1e9157f0214bb25121a11cc26a1f00
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:41:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/software_updates_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['software_updates_additional_updates'] = 'ಹೆಚ್ಚುವರಿ ನವೀಕರಣಗಳನ್ನು';
+$lang['software_updates_app_description'] = 'ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟ್ಗಳು ಅಪ್ಲಿಕೇಶನ್ ಆಧಾರವಾಗಿರುವ ಕಾರ್ಯಾಚರಣೆ ವ್ಯವಸ್ಥೆಯ ಘಟಕಗಳನ್ನು ನವೀಕರಣಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['software_updates_app_name'] = 'ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟ್ಗಳು';
+$lang['software_updates_automatic_updates'] = 'ಸ್ವಯಂಚಾಲಿತ ನವೀಕರಣಗಳನ್ನು';
+$lang['software_updates_available_updates'] = 'ಲಭ್ಯವಿರುವ ಅಪ್ಡೇಟ್ಗಳು';
+$lang['software_updates_busy_message'] = 'ಮತ್ತೊಂದು ಬಳಕೆದಾರ ಅಥವಾ ಸೇವೆಯನ್ನು ಪ್ರಸ್ತುತ ಸಾಫ್ಟ್ವೇರ್ ನವೀಕರಣಗಳನ್ನು ವ್ಯವಸ್ಥೆಯನ್ನು ಬಳಸುತ್ತಿದೆ. ಯಾವಾಗ ಪ್ರಕ್ರಿಯೆ ಪೂರ್ಣಗೊಂಡ, ಈ ಅಪ್ಲಿಕೇಶನ್ ಮತ್ತೆ ಲಭ್ಯವಾಗುತ್ತದೆ.';
+$lang['software_updates_current_progress'] = 'ಪ್ರಸ್ತುತ ಪ್ರಗತಿಯಲ್ಲಿರುವ';
+$lang['software_updates_details'] = 'ವಿವರಗಳು';
+$lang['software_updates_install_progress'] = 'ಪ್ರೋಗ್ರೆಸ್ ಅನುಸ್ಥಾಪಿಸಲು';
+$lang['software_updates_loading_updates_message'] = 'ಲೋಡ್ ತಂತ್ರಾಂಶ ನವೀಕರಣಗಳನ್ನು ಪಟ್ಟಿಯನ್ನು, ದಯವಿಟ್ಟು ತಾಳ್ಮೆಯಿಂದಿರಿ.';
+$lang['software_updates_no_data_available'] = 'ಯಾವುದೇ ಡೇಟಾ ಲಭ್ಯವಿಲ್ಲ.';
+$lang['software_updates_overall_progress'] = 'ಒಟ್ಟಾರೆ ಪ್ರೋಗ್ರೆಸ್';
+$lang['software_updates_package'] = 'ಪ್ಯಾಕೇಜ್';
+$lang['software_updates_please_be_patient_detail'] = 'ಇದು ಎಲ್ಲಾ ಭಂಡಾರ ಮತ್ತು ಅಪ್ಡೇಟ್ ವಿವರಗಳು ನವೀಕರಿಸಲು ಒಂದು ನಿಮಿಷ ಅಥವಾ ಎರಡು ತೆಗೆದುಕೊಳ್ಳಬಹುದು. ಈ ಮಾಹಿತಿಯನ್ನು ಮುಂದಿನ ಬಳಕೆಗೆ ಕ್ಯಾಶೆ ಆಗಿದೆ.';
+$lang['software_updates_please_be_patient'] = 'ದಯವಿಟ್ಟು ತಾಳ್ಮೆಯಿಂದಿರಿ!';
+$lang['software_updates_progress_help'] = 'ನಿಮ್ಮ ವ್ಯವಸ್ಥೆಯನ್ನು ಪ್ರಸಕ್ತ ಅಪ್ಡೇಟ್ ಮಾಡಲಾಗುತ್ತಿದೆ. ಈ ಪ್ರಕ್ರಿಯೆಯು ಸ್ವಲ್ಪ ಸಮಯ ತೆಗೆದುಕೊಳ್ಳಬಹುದು, ಆದ್ದರಿಂದ ದಯವಿಟ್ಟು ತಾಳ್ಮೆಯಿಂದಿರಿ!';
+$lang['software_updates_progress'] = 'ಪ್ರೋಗ್ರೆಸ್';
+$lang['software_updates_recent_software_activity'] = 'ಇತ್ತೀಚಿನ ತಂತ್ರಾಂಶ ಚಟುವಟಿಕೆ';
+$lang['software_updates_repository'] = 'ರೆಪೊಸಿಟರಿಯನ್ನು';
+$lang['software_updates_return_to_overview'] = 'ಅವಲೋಕನ ಹಿಂತಿರುಗಿ';
+$lang['software_updates_software_update_in_progress'] = 'ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟ್ ಪ್ರಗತಿಯಲ್ಲಿದೆ. ದಯವಿಟ್ಟು ನಂತರ ಹಿಂತಿರುಗಿ.';
+$lang['software_updates_software_updates_busy'] = 'ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟ್ಗಳು ಬ್ಯುಸಿ';
+$lang['software_updates_software_updates'] = 'ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟ್ಗಳು';
+$lang['software_updates_software_updates_system_busy'] = 'ಸಾಫ್ಟ್ವೇರ್ ನವೀಕರಣಗಳು ವ್ಯವಸ್ಥೆಯ ಕಾರ್ಯನಿರತವಾಗಿದೆ.';
+$lang['software_updates_software_up_to_date'] = 'ಅಪ್ ಯಾ ದಿನಾಂಕ ತಂತ್ರಾಂಶ';
+$lang['software_updates_system_is_up_to_date'] = 'ವ್ಯವಸ್ಥೆ ಅಪ್ ಯಾ ದಿನಾಂಕ.';
+$lang['software_updates_the_latest_software_updates_are_installed'] = 'ಇತ್ತೀಚಿನ ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟುಗಳನ್ನು ಸ್ಥಾಪಿಸಲಾಗಿದೆ.';
+$lang['software_updates_type'] = 'ಕೌಟುಂಬಿಕತೆ';
+$lang['software_updates_update_all'] = 'ಎಲ್ಲವನ್ನು ಆಧುನೀಕರಿಸು';
+$lang['software_updates_updates_complete'] = 'ತಂತ್ರಾಂಶ ಅಪ್ಡೇಟ್ಗಳು ಕಂಪ್ಲೀಟ್';
+$lang['software_updates_updates'] = 'ಅಪ್ಡೇಟ್ಗಳು';
+$lang['software_updates_upgrade'] = 'ಅಪ್ಗ್ರೇಡ್';
+$lang['software_updates_wizard_description'] = 'ಈಗ ನಿಮ್ಮ ನೆಟ್ವರ್ಕ್ ಕಾನ್ಫಿಗರ್, ಕೋರ್ ವ್ಯವಸ್ಥೆಗೆ ನಿರ್ಣಾಯಕ ನವೀಕರಣಗಳನ್ನು ಈಗ ಸ್ಥಾಪಿಸಲಾಗಿದೆ.';
+$lang['software_updates_wizard_latest_necessary'] = 'ಮುಂದುವರಿಯುವ ಮೊದಲು ನಿಮ್ಮ ಸಿಸ್ಟಮ್ ಸಾಫ್ಟ್ವೇರ್ ನವೀಕರಿಸಿ.';
